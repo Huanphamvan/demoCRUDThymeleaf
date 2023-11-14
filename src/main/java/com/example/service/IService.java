15ba5e1bb2107821422a755827c9c314565727b9
Huanphamvan <phamvanhuan.bk@gmail.com>
2023-11-14 13:37:42 +0900

    DemoCRUDThymeleaf not upload file

---
+++ src/main/java/com/example/service/IService.java
@@ -0,0 +1,12 @@
+package com.example.service;
+
+import java.util.List;
+
+public interface IService <E>{
+    List<E> findAll();
+    int findById(int id);
+    void add(E e);
+    void update(int id,E e);
+    void remove(int id);
+    List<E> findByName(String name);
+}
